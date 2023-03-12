b8b754f327ac3c0efa3a3cee02a77a05da789370
cquestor <3206293873@qq.com>
2023-03-12 18:45:51 +0800

    [NEW]上下文

--- context.go
+++ context.go
@@ -1,4 +1,67 @@
 package gone
 
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"sync"
+)
+
+// Context 上下文
 type Context struct {
+	Req      *http.Request
+	Resp     http.ResponseWriter
+	Method   string
+	Path     string
+	lockCode sync.Once
+}
+
+// newContext 构造上下文
+func newContext(w http.ResponseWriter, r *http.Request) *Context {
+	return &Context{
+		Req:      r,
+		Resp:     w,
+		Method:   r.Method,
+		Path:     r.URL.Path,
+		lockCode: sync.Once{},
+	}
+}
+
+// Header 获取请求头
+func (ctx *Context) Header(key string) string {
+	return ctx.Req.Header.Get(key)
+}
+
+// SetHeader 设置响应头
+func (ctx *Context) SetHeader(key, value string) {
+	ctx.Resp.Header().Set(key, value)
+}
+
+// Query 获取请求参数
+func (ctx *Context) Query(key string) string {
+	return ctx.Req.URL.Query().Get(key)
+}
+
+// PostFrom 获取表单参数
+func (ctx *Context) PostForm(key string) string {
+	return ctx.Req.PostFormValue(key)
+}
+
+// Body 获取请求体
+func (ctx *Context) Body() []byte {
+	b, _ := io.ReadAll(ctx.Req.Body)
+	ctx.Req.Body = io.NopCloser(bytes.NewBuffer(b))
+	return b
+}
+
+// Write 写入响应数据
+func (ctx *Context) Write(data []byte) {
+	ctx.Resp.Write(data)
+}
+
+// setStatusCode 设置响应状态码
+func (ctx *Context) setStatusCode(code int) {
+	ctx.lockCode.Do(func() {
+		ctx.Resp.WriteHeader(code)
+	})
 }
--- response.go
+++ response.go
@@ -1,5 +1,76 @@
 package gone
 
+import "fmt"
+
+// IResponse 响应接口
 type IResponse interface {
 	Invoke(ctx *Context)
 }
+
+// responseString 字符串响应
+type responseString struct {
+	StatusCode int
+	Data       string
+}
+
+// responseHtml 网页响应
+type responseHtml struct {
+	StatusCode int
+	Data       []byte
+}
+
+// responseJson json响应
+type responseJson struct {
+	StatusCode int
+	Data       any
+}
+
+// responseData 二进制响应
+type responseData struct {
+	StatusCode int
+	Data       []byte
+}
+
+// String 构造字符串响应
+func String(code int, format string, v ...any) *responseString {
+	return &responseString{
+		StatusCode: code,
+		Data:       fmt.Sprintf(format, v...),
+	}
+}
+
+// Html 构造网页响应
+func Html(code int, data []byte) *responseHtml {
+	return &responseHtml{
+		StatusCode: code,
+		Data:       data,
+	}
+}
+
+// Json 构造 json 响应
+func Json(code int, data any) *responseJson {
+	return &responseJson{
+		StatusCode: code,
+		Data:       data,
+	}
+}
+
+// Data 构造二进制响应
+func Data(code int, data []byte) *responseData {
+	return &responseData{
+		StatusCode: code,
+		Data:       data,
+	}
+}
+
+// Invoke 执行字符串响应
+func (response *responseString) Invoke(ctx *Context) {}
+
+// Invoke 执行网页响应
+func (response *responseHtml) Invoke(ctx *Context) {}
+
+// Invoke 执行 json 响应
+func (response *responseJson) Invoke(ctx *Context) {}
+
+// Invoke 执行二进制响应
+func (response *responseData) Invoke(ctx *Context) {}
